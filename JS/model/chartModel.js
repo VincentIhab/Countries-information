9d5edb88e79a98d06dfa3451c6e07907a1ce9d7e
sayedlehab <ricardo3u3@gmail.com>
2024-07-05 22:12:08 +0300

    adding new Apis

--- JS/model/chartModel.js
+++ JS/model/chartModel.js
@@ -1,57 +1,72 @@
 export default async function fetchData() {
-  //   const countryCode = document
-  //     .getElementById("countryInput")
-  //     .value.toUpperCase();
-  const gdpPerCapitaUrl = `https://api.worldbank.org/v2/country/${"EG"}/indicator/NY.GDP.PCAP.CD?format=json`;
-  const populationUrl = `https://api.worldbank.org/v2/country/${"EG"}/indicator/SP.POP.TOTL?format=json`;
-  const gdpGrowthRateUrl = `https://api.worldbank.org/v2/country/${"EG"}/indicator/NY.GDP.MKTP.KD.ZG?format=json`;
+  // const countryCode = document.getElementById('countryInput').value.toUpperCase();
+  const countryCode = "EG";
   const countryInfoUrl = `https://restcountries.com/v3.1/all`;
+  const urls = [
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NY.GDP.PCAP.CD?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/SP.POP.TOTL?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NY.GDP.MKTP.KD.ZG?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NY.GDP.MKTP.CD?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NE.EXP.GNFS.ZS?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NE.IMP.GNFS.ZS?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/SL.UEM.TOTL.ZS?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/FP.CPI.TOTL.ZG?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/SP.DYN.LE00.IN?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/SI.POV.DDAY?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NY.GNS.ICTR.ZS?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/SE.PRM.ENRR?format=json`,
+    `https://api.worldbank.org/v2/country/${countryCode}/indicator/NE.CON.GOVT.ZS?format=json`,
+  ];
 
   try {
-    const [
-      countryInfoResponse,
-      gdpPerCapitaResponse,
-      populationResponse,
-      gdpGrowthRateResponse,
-    ] = await Promise.all([
+    const [countryInfoResponse, ...responses] = await Promise.all([
       fetch(countryInfoUrl),
-      fetch(gdpPerCapitaUrl),
-      fetch(populationUrl),
-      fetch(gdpGrowthRateUrl),
+      ...urls.map((url) => fetch(url)),
     ]);
-
     const countryInfoData = await countryInfoResponse.json();
-    const gdpPerCapitaData = await gdpPerCapitaResponse.json();
-    const populationData = await populationResponse.json();
-    const gdpGrowthRateData = await gdpGrowthRateResponse.json();
-
-    const countryInfo = countryInfoData.find(
-      (country) => country.cca2 === "EG"
+    const data = await Promise.all(
+      responses.map((response) => response.json())
     );
 
-    if (
-      countryInfo &&
-      gdpPerCapitaData[1] &&
-      populationData[1] &&
-      gdpGrowthRateData[1]
-    ) {
-      const years = gdpPerCapitaData[1].map((entry) => entry.date).reverse();
-      const gdpPerCapitaValues = gdpPerCapitaData[1]
+    const countryInfo = countryInfoData.find(
+      (country) => country.cca2 === countryCode
+    );
+    if (countryInfo && data.every((d) => d[1])) {
+      const years = data[0][1].map((entry) => entry.date).reverse();
+      const gdpPerCapita = data[0][1].map((entry) => entry.value).reverse();
+      const population = data[1][1].map((entry) => entry.value).reverse();
+      const gdpGrowthRate = data[2][1].map((entry) => entry.value).reverse();
+      const gdp = data[3][1].map((entry) => entry.value).reverse();
+      const exports = data[4][1].map((entry) => entry.value).reverse();
+      const imports = data[5][1].map((entry) => entry.value).reverse();
+      const unemployment = data[6][1].map((entry) => entry.value).reverse();
+      const inflation = data[7][1].map((entry) => entry.value).reverse();
+      const lifeExpectancy = data[8][1].map((entry) => entry.value).reverse();
+      const poverty = data[9][1].map((entry) => entry.value).reverse();
+      const grossSavings = data[10][1].map((entry) => entry.value).reverse();
+      const schoolEnrollment = data[11][1]
         .map((entry) => entry.value)
         .reverse();
-      const populationValues = populationData[1]
-        .map((entry) => entry.value)
-        .reverse();
-      const gdpGrowthRateValues = gdpGrowthRateData[1]
+      const governmentExpenditure = data[12][1]
         .map((entry) => entry.value)
         .reverse();
 
-      //   displayCountryInfo(countryInfo);
+      // displayCountryInfo(countryInfo);
       createChart(
         years,
-        gdpPerCapitaValues,
-        populationValues,
-        gdpGrowthRateValues
+        gdpPerCapita,
+        population,
+        gdpGrowthRate,
+        gdp,
+        exports,
+        imports,
+        unemployment,
+        inflation,
+        lifeExpectancy,
+        poverty,
+        grossSavings,
+        schoolEnrollment,
+        governmentExpenditure
       );
     } else {
       alert("No data available for the entered country code.");
@@ -62,17 +77,31 @@ export default async function fetchData() {
 }
 
 // function displayCountryInfo(countryInfo) {
-//   const infoDiv = document.getElementById("countryInfo");
-//   infoDiv.innerHTML = `
-//     <h2>${countryInfo.name.common}</h2>
-//     <p>Capital: ${countryInfo.capital[0]}</p>
-//     <p>Region: ${countryInfo.region}</p>
-// `;
+//     const infoDiv = document.getElementById('countryInfo');
+//     infoDiv.innerHTML = `
+//         <h2>${countryInfo.name.common}</h2>
+//         <p>Capital: ${countryInfo.capital[0]}</p>
+//         <p>Region: ${countryInfo.region}</p>
+//     `;
 // }
-
-function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
+function createChart(
+  years,
+  gdpPerCapita,
+  population,
+  gdpGrowthRate,
+  gdp,
+  exports,
+  imports,
+  unemployment,
+  inflation,
+  lifeExpectancy,
+  poverty,
+  grossSavings,
+  schoolEnrollment,
+  governmentExpenditure
+) {
   const ctx = document.getElementById("economicChart").getContext("2d");
-  new Chart(ctx, {
+  const chart = new Chart(ctx, {
     type: "line",
     data: {
       labels: years,
@@ -80,7 +109,111 @@ function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
         {
           label: "GDP per Capita (current US$)",
           data: gdpPerCapita,
-          borderColor: "rgba(75, 192, 192, 1)",
+          borderColor: "#3e95cd",
+          backgroundColor: "rgba(62, 149, 205, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "rectRot",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: false, // Initially show this dataset
+        },
+        {
+          label: "Population",
+          data: population,
+          borderColor: "#8e5ea2",
+          backgroundColor: "rgba(142, 94, 162, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "triangle",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: false, // Initially show this dataset
+        },
+        {
+          label: "GDP Growth Rate (%)",
+          data: gdpGrowthRate,
+          borderColor: "#3cba9f",
+          backgroundColor: "rgba(60, 186, 159, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "circle",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "GDP (current US$)",
+          data: gdp,
+          borderColor: "#e8c3b9",
+          backgroundColor: "rgba(232, 195, 185, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "cross",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "Exports of goods and services (% of GDP)",
+          data: exports,
+          borderColor: "#c45850",
+          backgroundColor: "rgba(196, 88, 80, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "star",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "Imports of goods and services (% of GDP)",
+          data: imports,
+          borderColor: "#ff6384",
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "rect",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "Unemployment Rate (%)",
+          data: unemployment,
+          borderColor: "#36a2eb",
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "triangle",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "Inflation Rate (%)",
+          data: inflation,
+          borderColor: "#ffce56",
+          backgroundColor: "rgba(255, 206, 86, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "circle",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "Life Expectancy (years)",
+          data: lifeExpectancy,
+          borderColor: "#4bc0c0",
           backgroundColor: "rgba(75, 192, 192, 0.2)",
           borderWidth: 2,
           fill: true,
@@ -88,30 +221,59 @@ function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
           pointStyle: "rectRot",
           pointRadius: 5,
           pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
         },
         {
-          label: "Population",
-          data: population,
-          borderColor: "rgba(255, 99, 132, 1)",
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          label: "Poverty Headcount Ratio (% of population)",
+          data: poverty,
+          borderColor: "#9966ff",
+          backgroundColor: "rgba(153, 102, 255, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "star",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "Gross Savings (% of GDP)",
+          data: grossSavings,
+          borderColor: "#ff9f40",
+          backgroundColor: "rgba(255, 159, 64, 0.2)",
+          borderWidth: 2,
+          fill: true,
+          tension: 0.4,
+          pointStyle: "crossRot",
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
+        },
+        {
+          label: "School Enrollment (%)",
+          data: schoolEnrollment,
+          borderColor: "#e74c3c",
+          backgroundColor: "rgba(231, 76, 60, 0.2)",
           borderWidth: 2,
           fill: true,
           tension: 0.4,
           pointStyle: "triangle",
           pointRadius: 5,
           pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
         },
         {
-          label: "GDP Growth Rate (%)",
-          data: gdpGrowthRate,
-          borderColor: "rgba(54, 162, 235, 1)",
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          label: "Government Expenditure (% of GDP)",
+          data: governmentExpenditure,
+          borderColor: "#27ae60",
+          backgroundColor: "rgba(39, 174, 96, 0.2)",
           borderWidth: 2,
           fill: true,
           tension: 0.4,
-          pointStyle: "circle",
+          pointStyle: "rect",
           pointRadius: 5,
           pointHoverRadius: 8,
+          hidden: true, // Initially hide this dataset
         },
       ],
     },
@@ -123,10 +285,9 @@ function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
           position: "top",
           labels: {
             font: {
-              size: 14,
+              size: 16,
             },
-            color: "#eee",
-            padding: 25,
+            color: "#f0f0f0",
           },
         },
         tooltip: {
@@ -134,8 +295,6 @@ function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
           mode: "index",
           intersect: false,
           backgroundColor: "rgba(0, 0, 0, 0.8)",
-          padding: 15,
-          paddingTop: 105,
           titleFont: {
             size: 16,
           },
@@ -158,25 +317,25 @@ function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
         x: {
           grid: {
             display: true,
-            color: "rgba(200, 200, 200, 0.2)",
+            color: "rgba(240, 240, 240, 0.2)",
           },
           ticks: {
             font: {
               size: 14,
             },
-            color: "#ccc",
+            color: "#f0f0f0",
           },
         },
         y: {
           grid: {
             display: true,
-            color: "rgba(200, 200, 200, 0.2)",
+            color: "rgba(240, 240, 240, 0.2)",
           },
           ticks: {
             font: {
               size: 14,
             },
-            color: "#ccc",
+            color: "#f0f0f0",
           },
         },
       },
@@ -186,5 +345,35 @@ function createChart(years, gdpPerCapita, population, gdpGrowthRate) {
       },
     },
   });
+
+  // Create UI controls for dataset visibility
+  const checkboxes = document.createElement("div");
+  checkboxes.classList.add("checkboxes-container");
+
+  chart.data.datasets.forEach((dataset, index) => {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = `dataset-${index}`;
+    checkbox.checked = !dataset.hidden; // Reflect dataset visibility
+    checkbox.addEventListener("change", function () {
+      chart.getDatasetMeta(index).hidden = !this.checked;
+      chart.update();
+    });
+
+    const label = document.createElement("label");
+    label.htmlFor = `dataset-${index}`;
+    label.textContent = dataset.label;
+
+    const checkboxContainer = document.createElement("div");
+    checkboxContainer.classList.add("checkbox-container");
+    checkboxContainer.appendChild(checkbox);
+    checkboxContainer.appendChild(label);
+
+    checkboxes.appendChild(checkboxContainer);
+  });
+
+  // Add checkboxes container to the chart container
+  const chartContainer = document.getElementById("chartContainer");
+  chartContainer.appendChild(checkboxes);
 }
 fetchData();
--- app.js
+++ app.js
@@ -4,8 +4,10 @@
 import WeatherController from "/JS/controllers/WeatherController.js";
 import SuggestionController from "/JS/controllers/SuggestionController.js";
 import chart from "/JS/model/chartModel.js";
+import mapModel from "./JS/model/mapModel.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // mapModel.getPosition();
   const weatherController = new WeatherController();
   const suggestionController = new SuggestionController();
   fetch("http://api.geonames.org/getJSON?geonameId=360630&username=sayed_rhab")
@@ -14,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((err) => console.error(err));
 
   const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&titles=${"suez"}&origin=*`;
-
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -27,27 +28,20 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Error fetching Wikipedia article:", error)
     );
 
-  const countryCode = "EG";
-  const indicators = ["NY.GDP.MKTP.CD", "SP.POP.TOTL"]; // GDP and Population
-  const fetchData = async () => {
-    try {
-      const promises = indicators.map((indicator) =>
-        fetch(
-          `http://api.worldbank.org/v2/country/${countryCode}/indicator/${indicator}?format=json`
-        ).then((response) => response.json())
-      );
-      const results = await Promise.all(promises);
-      results.forEach((data) => {
-        if (data && data[1]) {
-          console.log(`${data[1][0].indicator.value} Data:`, data[1]);
-        } else {
-          console.error("No data found for one of the indicators.");
-        }
-      });
-    } catch (error) {
-      console.error("Error fetching World Bank data:", error);
-    }
-  };
+  const apiKey = "e44e2e87863b4f50b4c8de75b7ac1422";
+  const country = "US";
+  const url2 = `https://newsapi.org/v2/everything?q=${country}&apiKey=${apiKey}`;
 
-  fetchData();
+  fetch(url2)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.articles);
+      data.articles.forEach((article) => {
+        console.log(`Title: ${article.title}`);
+        console.log(`Description: ${article.description}`);
+        console.log(`URL: ${article.url}`);
+        console.log("---");
+      });
+    })
+    .catch((error) => console.error("Error:", error));
 });
